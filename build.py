998e97986a0cea5cbf765ca05658f4b3e74214f9
Chris Marsh <chrismarsh.c2@gmail.com>
2021-03-15 11:58:11 -0600

    Per OS detection and packaging

--- build.py
+++ build.py
@@ -5,22 +5,9 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(cppstds=[14],
                                 archs=["x86_64"],
                                 build_types=["Release"])
-                              
-    builder.add_common_builds(pure_c=False,shared_option_name="gsl:shared")
+                        
+    builder.add_common_builds(pure_c=False, shared_option_name=False)
+    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++") # old ABI
 
-    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
-
-    named_builds = defaultdict(list)
-    for settings, options, env_vars, build_requires, reference in builder.items:
-
-        shared="shared"
-
-        if not options['gsl:shared']:
-            shared = "static" 
-
-        named_builds[settings['compiler'] +"_"+shared].append([settings, options, env_vars, build_requires, reference])
-
-    builder.named_builds = named_builds
 
     builder.run()
-
